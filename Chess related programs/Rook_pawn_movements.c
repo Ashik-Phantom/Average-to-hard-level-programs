ec4884c39d4f3e659a40b7e73843ec64f75a6051
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2020-08-09 20:22:49 +0530

    Update Rook_pawn_movements.c

--- Chess related programs/Rook_pawn_movements.c
+++ Chess related programs/Rook_pawn_movements.c
@@ -8,25 +8,55 @@ a b
  
 output 
 count of movements 
-
+--------------------------------------------
 Testcase 1:
 input:
 8 8 
 6 5 
 2 5 
 Output:
+12
 
 Explaination:
+8X8 tiles 
+  0 1 2 3 4 5 6 7
+0 . . . . . . . . 
+1 . . . . . . . . 
+2 . . . . . P . . 
+3 . . . . . * . . 
+4 . . . . . * . . 
+5 . . . . . * . . 
+6 * * * * * R * * 
+7 . . . . . * . . 
 
-. . . . . . . .
-. . . . P . . .
-. . . . * . . .
-. . . . * . . .
-. . . . * . . .
-* * * * R * * *
-. . . . * . . .
-. . . . * . . .
+no of stars = output
+-------------------------------------------
+Testcase 2:
+Input:
+5 4 
+4 3
+4 2
+
+Output 
+4
+
+Explaination:
+5X4 tiles 
+
+Explaination:
+5X4 tiles 
+  0 1 2 3
+0 . . . *
+1 . . . *
+2 . . . *
+3 . . . *
+4 . . P R  
+
+no of stars = output
+
 */
+//Solution
+
 #include<stdio.h>
 #include <stdlib.h>
 
