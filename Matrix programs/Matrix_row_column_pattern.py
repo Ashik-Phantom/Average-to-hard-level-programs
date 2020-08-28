aa2f958402b982fa268e973141cecf8460d218a4
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2020-08-28 09:29:44 +0530

    Update Matrix_row_column_pattern.py

--- Matrix programs/Matrix_row_column_pattern.py
+++ Matrix programs/Matrix_row_column_pattern.py
@@ -1,2 +1,8 @@
 # Matrix Row - column pattern
 
+n,m=map(int,input().split()) 
+l=[list(map(int,input().split())) for i in range(n)] 
+while l: 
+    print(*l[0]) 
+    l=l[1:] 
+    l=[list(i) for i in zip(*l)]
