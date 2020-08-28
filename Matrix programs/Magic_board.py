682e591ef012aab872a5722f3add1071046ec16a
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2020-08-28 12:20:06 +0530

    Update Magic_board.py

--- Matrix programs/Magic_board.py
+++ Matrix programs/Magic_board.py
@@ -1,6 +1,4 @@
-# (incorrect)
-
-m, n=map(int, input().split())
+m, n = map(int, input().split())
 no_of_reds = int(input())
 reds, blues = [], []
 for i in range(no_of_reds):
@@ -11,11 +9,11 @@ for i in range(no_of_reds):
 no_of_blues = int(input())
 for i in range(no_of_blues):
     A = input().strip()
-    x, y=A.split()
+    x, y = A.split()
     f = [int(x),int(y)]
     blues.append(f)
 moves = int(input())
-board = [['0' for i in range(m)] for j in range(n)]
+board = [['0' for i in range(n)] for j in range(m)]
 for x in reds:
     a, b = x[0]-1, x[1]-1
     for i in range(m):
@@ -24,7 +22,7 @@ for x in reds:
                 board[i][j] = '1R'
                 break
 for x in blues:
-    a,b = x[0]-1, x[1]-1
+    a, b = x[0]-1, x[1]-1
     for i in range(m):
         for j in range(n):
             if i == a and j == b:
@@ -52,21 +50,21 @@ for x in range(moves):
                             board[i - 1][j] = str(int(board[i - 1][j][0]) + int(board[i][j][0])) + 'R'
                             board[i][j] = '0'
     else:
-        for i in range(m):
-            for j in range(n):
-                if 'B' in board[i][j]:
-                    if j < n-1:
-                        if 'B' in board[i][j+1]:
-                            board[i][j+1] = str(int(board[i][j+1][0]) + int(board[i][j][0])) + 'B'
-                            board[i][j] = '0'
-                        elif board[i][j+1] == '0':
-                            board[i][j+1] = board[i][j][0]+'B'
-                            board[i][j] = '0'
-                        elif 'R' in board[i][j+1]:
-                            board[i][j+1] = str(int(board[i][j+1][0]) + int(board[i][j][0])) + 'R'
-                            board[i][j] = '0'
-    for i in board:
-        print(*i)
+        for ii in range(m-1,-1,-1):
+            for jj in range(n-1,-1,-1):
+                if 'B' in board[ii][jj]:
+                    if jj < n-1:
+                        if 'B' in board[ii][jj+1]:
+                            board[ii][jj+1] = str(int(board[ii][jj+1][0]) + int(board[ii][jj][0])) + 'B'
+                            board[ii][jj] = '0'
+                        elif board[ii][jj+1] == '0':
+                            board[ii][jj+1] = board[ii][jj][0]+'B'
+                            board[ii][jj] = '0'
+                        elif 'R' in board[ii][jj+1]:
+                            board[ii][jj+1] = str(int(board[ii][jj+1][0]) + int(board[ii][jj][0])) + 'R'
+                            board[ii][jj] = '0'
+    for iii in board:
+        print(*iii)
     print()
 print()
 print("Final Answer:")
@@ -102,4 +100,32 @@ Output:
 0 0 1B 0 0 0 0 0
 0 0 0 0 0 0 0 0
 0 0 0 0 0 0 0 0
+
+Test case 2:
+4 5 
+6
+4 1
+3 2 
+3 1
+1 1 
+2 1
+3 5 
+10 
+2 2 
+4 4
+2 4
+1 5 
+2 5 
+1 4
+4 3 
+4 5 
+1 3
+2 3
+3 
+
+Output:
+3R 2R 0 1B 5R
+1R 0 0 1B 0
+0 0 0 0 0
+0 0 0 1B 2B
 """
