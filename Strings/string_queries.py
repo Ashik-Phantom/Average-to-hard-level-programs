fc5c8c44a76f96a569bb688a18c405faaa9d58ba
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2020-08-28 10:07:38 +0530

    Update string_queries.py

--- Strings/string_queries.py
+++ Strings/string_queries.py
@@ -1,12 +1,26 @@
 #String -Queries
+"""
+Example Input/Output:
+Input:
+skillrack 
+5
+s+1 
+2+s 
+s+4 
+s+1 
+3+s
 
+Output:
+sksskskillrackkackkk
+"""
+# Solution
 s = input().strip()
 n = int(input()) 
 for i in range(n):
-    S=input().strip() 
-    x,y=S.split('+') 
+    S = input().strip() 
+    x, y = S.split('+') 
     if y.isdigit():
-        s=s+s[-int(y):]
+        s = s + s[-int(y):]
     else:
-        s=s[:int(x)]+s
+        s = s[:int(x)] + s
 print(s)    
