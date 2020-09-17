aab794715f36f95002e7bf04a41648a6abf16e87
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2020-09-17 22:31:14 +0530

    Create longest_possible_substring.py
    to be updated

---
+++ Strings/longest_possible_substring.py
@@ -0,0 +1,22 @@
+# longest possible substring
+"""
+Example Input/Output 1:
+Input:
+aaabbccc
+Output:
+aaa ccc
+explanation:
+In the given string aaabbccc, the longest possible substring values that are formed using a character are aaa and ccc.
+Hence the output is
+aaa ccc
+Example Input/Output 2:
+Input:
+XXyyyy33333xxxZZzzzAaAaA
+Output:
+33333 ZZzzz AaAaA
+Example Input/Output 3:
+Input:
+pqrs@87
+Output:
+-1
+"""
