f7db3494e75b034e9db507f3e1af35c7ce0d0699
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2021-07-23 16:49:21 +0530

    Update find_max_value.java

--- Dynamic_programing/0 by 1 Knapsack Problems/find_max_value.java
+++ Dynamic_programing/0 by 1 Knapsack Problems/find_max_value.java
@@ -1,3 +1,9 @@
+/*
+Given a knapsack with capacity C and two arrays w[] and val[] representing the weights and values of N distinct items, 
+the task is to find the maximum value you can put into the knapsack.
+Items cannot be broken and an item with weight X takes X capacity of the knapsack.
+*/
+
 import java.util.*;
 
 import javax.lang.model.util.ElementScanner6;
