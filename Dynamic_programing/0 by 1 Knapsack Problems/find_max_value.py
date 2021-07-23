33180eb6e1b95c384c028a967763b786dc63d708
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2021-07-23 16:46:22 +0530

    Create find_max_value.py

---
+++ Dynamic_programing/0 by 1 Knapsack Problems/find_max_value.py
@@ -0,0 +1,29 @@
+n, w = map(int,input().split()) 
+wt = list(map(int,input().split())) 
+val = list(map(int,input().split())) 
+r,c = n+1,w+1
+dp = [[0 for i in range(c)] for j in range(r)]
+for i in range(r):
+    for j in range(c):
+        if i==0 or j==0:
+            dp[i][j] = 0 
+        elif j>=wt[i-1]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-wt[i-1]] + val[i-1])
+        else:
+            dp[i][j] = dp[i-1][j] 
+for i in dp:
+    for j in i:
+        print(j,end=" ")
+    print()
+
+
+"""
+4 8 
+2 5 4 3
+3 6 7 2
+ans 10
+6 9
+3 1 5 8 4 2
+10 8 19 15 20 16
+ans 46
+"""
