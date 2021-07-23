7f04842b646a60d047d1f16e5d86da79d2ab4855
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2021-07-23 16:45:21 +0530

    Create find_max_value.java

---
+++ Dynamic_programing/0 by 1 Knapsack Problems/find_max_value.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+import javax.lang.model.util.ElementScanner6;
+public class knapsack
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int C = sc.nextInt();
+        int wt[] = new int[N];
+        int val[] = new int[N];
+        int dp[][] = new int[N+1][C+1];
+        for(int i=0;i<N;i++)
+        {
+            wt[i] = sc.nextInt();
+        }
+        for(int i=0;i<N;i++)
+        {
+            val[i] = sc.nextInt();
+        }
+        for(int i=0;i<=N;i++)
+        {
+            for(int j=0;j<=C;j++)
+            {
+                if(i==0 || j==0) dp[i][j] = 0;
+                else if(wt[i-1]<=j)
+                {
+                    dp[i][j] = Math.max((dp[i-1][j-wt[i-1]]+val[i-1]),dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        for(int i=0;i<=N;i++)
+        {
+            for(int j=0;j<=C;j++)
+            {
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+
