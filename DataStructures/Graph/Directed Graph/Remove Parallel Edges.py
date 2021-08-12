477f3f7f7629355991cb58eb7a22a0b3e49729df
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2021-08-12 15:43:15 +0530

    Update Remove Parallel Edges.py

--- DataStructures/Graph/Directed Graph/Remove Parallel Edges.py
+++ DataStructures/Graph/Directed Graph/Remove Parallel Edges.py
@@ -67,4 +67,15 @@ Output:
 8 4 0 0 0 0 
 """
 
-
+n=int(input())
+l=[list(map(int,input().split())) for i in range(n)]
+for i in range(n):
+    for j in range(n):
+        if l[i][j]!=0 and l[j][i]!=0:
+            if l[i][j]>l[j][i]:
+                l[i][j]=0
+            else:
+                l[j][i]=0
+for i in l:
+    print(*i)
+    
